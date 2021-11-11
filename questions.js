1a48ebbcf9c0766154de65b3413970fb2fe80f52
Pyroblocks <68356572+Pyroblocks@users.noreply.github.com>
2021-11-10 18:51:18 -0600

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,5 +1,5 @@
 questions =  [
     {"id": 0, "question": "Modernist", "effects": {"mod": 1}},
-    {"id": 1, "question": "Technoist", "effects": {"tec": 1}},
+    {"id": 1, "question": "Federalist", "effects": {"fed": 1}},
     {"id": 2, "question": "Alianzen", "effects": {"ali": 1}},
 ];
