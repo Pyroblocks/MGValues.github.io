68bbc6bc3aefa2b514e121745838467778ddcaed
Pyroblocks <68356572+Pyroblocks@users.noreply.github.com>
2021-11-12 21:17:02 -0600

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,5 +1,17 @@
 questions =  [
-    {"id": 0, "question": "Modernist", "effects": {"mod": 1}},
-    {"id": 1, "question": "Federalist", "effects": {"fed": 1}},
-    {"id": 2, "question": "Alianzen", "effects": {"ali": 1}},
+    {"id": 0, "question": "Those who recieve punishments should not have what they were punished for held against them after they've served their punishment.", "effects": {"mod": -1}},
+    {"id": 1, "question": "The idea of Regional Courts is flawed.", "effects": {"fed": -1}},
+    {"id": 2, "question": "We should not be afriad to implement taxes if necessary.", "effects": {"ali": -1}},
+    
+    {"id": 3, "question": "The House of Representatives should be the only legislative body that may vote on bills.", "effects": {"mod": 1}},
+    {"id": 4, "question": "The general community should be prioritized over individual rights.", "effects": {"fed": 1}},
+    {"id": 5, "question": "Integrating the economy further into the Mock Government will create economic activity.", "effects": {"ali": 1}},
+    
+    {"id": 6, "question": "Reform should be the top priority of any Presidential administration.", "effects": {"mod": -1}},
+    {"id": 7, "question": "The electoral voting system has failed without any way of redemption.", "effects": {"fed": -1}},
+    {"id": 8, "question": "Mock Government Businesses should pay a portion of their profit in taxes to the treasury.", "effects": {"ali": -1}},
+    
+    {"id": 9, "question": "Court discussion channels should be private.", "effects": {"mod": 1}},
+    {"id": 10, "question": "More pre-reset parts of the server should be added.", "effects": {"fed": 1}},
+    {"id": 11, "question": "Economic activity will be better sparked by promoting individual activity than by instating group policies such as taxation.", "effects": {"fed": 1}},
 ];
